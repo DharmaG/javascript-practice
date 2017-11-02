606c8ec7be95887a6b119efec50d9fbfa46f90e4
Dharma Velasco <dharma.giselle@gmail.com>
2017-11-01 21:34:14 -0400

    arrays and objects

---
+++ script/main.js
@@ -0,0 +1,50 @@
+var beginningScenarios = ["You wake up in a hospotal. It is eerily quiet. you tiptoe to the door and peek out.",
+      "You are standing in an open field west of a white house with a boarded front door. There is a samall mailbox here.",
+      "Desperate times call for desperate measures. You see a small convenience store up ahead and decide to loot it for goods. "
+    ];
+
+  function randomNumber(range){
+    return Math.floor(Math.random() * range);
+
+  }
+
+  alert(beginningScenarios[randomNumber(beginningScenarios.length )]);
+
+
+var weaponList = ["shovel", "crossbow", "baseball bat", "rubber chicken"];
+
+var weapon = weaponList[randomNumber(weaponList.length)];
+alert("Being that it is thezombie apocalypse, you decide to search for a weapon first. After surveing your surroundings you notice and grab a  "
++ weapon + ".");
+
+alert("Suddenly a zombie bursts through the door! You ready your " + weapon + " and advance towrds the zombie.");
+
+var survival = randomNumber(2);
+
+if(survival === 0) {
+  alert("The zombie bites you. You lose!!");
+} else if (survival > 0) {
+  alert("You killthe zombie with your" + weapon + " You win !!!!");
+}
+
+
+// var mark = {
+//   name: "Mark",
+//   tshirtColor: "navy blue"
+// };
+//
+// var lisa = {
+//   name: "Lisa",
+//   tshirtColor: "red"
+// }
+
+function Friend(name, tshirtColor) {
+  this.name = name;
+  this.tshirtColor = tshirtColor;
+}
+
+var denny = new Friend("Denny", "green");
+var lisa = new Friend("Lisa", "red");
+
+alert("Hi my name is " + denny.name + ". My favorite color is "+ denny.tshirtColor);
+alert("Hi my name is " + lisa.name + ". My favorite color is "+ lisa.tshirtColor);
